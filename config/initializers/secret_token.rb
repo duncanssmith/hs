308c0545133d82fea988b543d02fccb536184ccd
Duncan Smith <duncanssmith@gmail.com>
2010-09-23 13:45:17 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Headshift::Application.config.secret_token = '383e5dff1933523a0c1f4fbed9c83968640af5882fe02c3f0984587ab595d9dc0c5c6d6de1f5e5020e107d3f47b8f2a2705520eb671b651d0b86083095b5889a'
